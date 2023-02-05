b7694eb7b8b10b4d6664fd7f7f1b037135a46201
Javier Segovia Córdoba <javier@segoviacordoba.com>
2023-02-05 02:53:02 +0100

    Wip 9

--- theme-javiersc/main/kotlin/Main.kt
+++ theme-javiersc/main/kotlin/Main.kt
@@ -1,12 +1,12 @@
-package com.javiersc
-
-fun main() {
-    hello()
-    Main()
-}
-
-fun hello(a: Int) {
-
-}
-
-data class Main(val aaaa: String)
+// package com.javiersc
+//
+// fun main() {
+//    hello()
+//    Main()
+// }
+//
+// fun hello(a: Int) {
+//
+// }
+//
+// data class Main(val aaaa: String)
